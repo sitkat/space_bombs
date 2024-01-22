50e9d5d59b7b5b369a3e5b023e4da276f49519ab
Sitnikova Yekaterina <sitkat03@mail.ru>
2024-01-22 19:23:12 +0300

    feat: add showAlertDialogs

--- lib/ui/game_screen.dart
+++ lib/ui/game_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:space_bombs/data/level_state.dart';
 import 'package:space_bombs/theme/app_colors.dart';
 import 'package:space_bombs/theme/app_container.dart';
+import 'package:space_bombs/ui/main_window.dart';
 
 import '../data/game_provider.dart';
 
@@ -80,7 +81,7 @@ class _GameScreenState extends State<GameScreen> {
                           child: Row(
                             children: [
                               _buildContainer(AppColors.accent, 'Level'),
-                              _buildPadding('1 / 5'),
+                              _buildPadding('${level.numberOfBombs} / 3'),
                             ],
                           ),
                         ),
@@ -130,6 +131,9 @@ class _GameScreenState extends State<GameScreen> {
       setState(() {
         if (remainingTime.inSeconds > 0) {
           remainingTime -= const Duration(seconds: 1);
+        } else {
+          _timer?.cancel();
+          showAlertDialogTimeOver();
         }
       });
     });
@@ -156,14 +160,21 @@ class _GameScreenState extends State<GameScreen> {
         if (isBomb[index]) {
           foundBomb++;
           if (foundBomb ==
-              GameProvider.getGameState(widget.state).numberOfBombs)
-            showAlertDialog();
+              GameProvider.getGameState(widget.state).numberOfBombs){
+            _timer?.cancel();
+            if (widget.state != LevelState.hard){
+              showAlertDialogWin();
+            } else {
+              showAlertDialogWinHard();
+            }
+          }
+
         }
       }
     });
   }
 
-  void showAlertDialog() {
+  void showAlertDialogWin() {
     showDialog(
       context: context,
       barrierDismissible: false,
@@ -223,7 +234,7 @@ class _GameScreenState extends State<GameScreen> {
                           color: GameProvider.getGameState(widget.state)
                               .backgroundColor),
                     ),
-                    const TextSpan(text: '\nlevel difficulty level.'),
+                    const TextSpan(text: '\ndifficulty level.'),
                   ],
                 ),
               ),
@@ -234,9 +245,204 @@ class _GameScreenState extends State<GameScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                _builtButton(() {}, 'Back to menu'),
+                _builtButton(() {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainWindow()),
+                  );
+                }, 'Back to menu'),
                 const SizedBox(width: 10),
-                _builtButton(() {}, 'The next difficulty'),
+                _builtButton(() {
+                  late LevelState nextLvl;
+                  if (widget.state == LevelState.easy){
+                    nextLvl = LevelState.normal;
+                  }
+                  if (widget.state == LevelState.normal){
+                    nextLvl = LevelState.hard;
+                  }
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GameScreen(state: nextLvl)),
+                  );
+                }, 'The next difficulty'),
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showAlertDialogWinHard() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: AppColors.card,
+          title: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/emptyBomb.png',
+                    width: 54,
+                  ),
+                  Image.asset(
+                    'assets/images/emptyBomb.png',
+                    width: 54,
+                  ),
+                  Image.asset(
+                    'assets/images/emptyBomb.png',
+                    width: 54,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'You found all the bombs.',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.whiteWithOpacity,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Level complete',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.white,
+                ),
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.whiteWithOpacity,
+                  ),
+                  children: [
+                    const TextSpan(text: 'You’ve passed all the '),
+                    const TextSpan(text: '\nlevels. Start again?'),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+            ],
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _builtButton(() {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainWindow()),
+                  );
+                }, 'Back to menu'),
+                const SizedBox(width: 10),
+                _builtButton(() {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GameScreen(state: LevelState.easy)),
+                  );
+                }, 'Restart'),
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showAlertDialogTimeOver() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: AppColors.card,
+          title: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/emptyBomb.png',
+                    width: 54,
+                  ),
+                  Image.asset(
+                    'assets/images/emptyBomb.png',
+                    width: 54,
+                  ),
+                  Image.asset(
+                    'assets/images/emptyBomb.png',
+                    width: 54,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'You didn\'t have time.',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.whiteWithOpacity,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Time is over',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.white,
+                ),
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.whiteWithOpacity,
+                  ),
+                  children: [
+                    const TextSpan(text: 'You’ve not completed the '),
+                    TextSpan(
+                      text: GameProvider.getGameState(widget.state).text,
+                      style: TextStyle(
+                          color: GameProvider.getGameState(widget.state)
+                              .backgroundColor),
+                    ),
+                    const TextSpan(text: '\ndifficulty level.'),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+            ],
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _builtButton(() {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainWindow()),
+                  );
+                }, 'Back to menu'),
+                const SizedBox(width: 10),
+                _builtButton(() {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GameScreen(state: widget.state)),
+                  );
+                }, 'Restart'),
               ],
             ),
           ],
